0d64aff6237c69b9c084959f8f491679ab21c3c6
oliviervth <olivier.vth@gmail.com>
2026-08-13 10:12:25 +0200

    Update download_statbel_vastgoedprijzen.py

--- scripts/download_statbel_vastgoedprijzen.py
+++ scripts/download_statbel_vastgoedprijzen.py
@@ -1,1 +1,31 @@
+import json
+import requests
+from pathlib import Path
 
+EXPORT_URL = "https://bestat.statbel.fgov.be/bestat/api/views/ddef4721-523c-4ac2-a595-315d7bd0496a/result/JSON "
+
+OUTPUT_DIR = Path("data")
+OUTPUT_DIR.mkdir(exist_ok=True)
+
+OUTPUT_FILE = OUTPUT_DIR / "vastgoedprijzen.json"
+
+def download():
+    print("Downloading Statbel income data...")
+  
+    response = requests.get(EXPORT_URL, timeout=120)
+    response.raise_for_status()
+
+    data = response.json()
+
+    with open(OUTPUT_FILE,"w",encoding="utf-8") as f:
+        json.dump(
+            data,
+            f,
+            ensure_ascii=False,
+            indent=2
+        )
+
+    print(f"Saved to {OUTPUT_FILE}")
+
+if __name__ == "__main__":
+        download()
