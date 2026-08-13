2c2064a9bf33504b5a09b2bf2e492a51753c2edf
oliviervth <olivier.vth@gmail.com>
2026-08-13 14:22:33 +0200

    Create download_statbel_leeftijd.py

---
+++ scripts/download_statbel_leeftijd.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from pathlib import Path
+
+EXPORT_URL = "https://bestat.statbel.fgov.be/bestat/api/views/08f8d45a-9053-44b6-b34d-29a0e98f1e5a/result/JSON"
+
+OUTPUT_DIR = Path("data")
+OUTPUT_DIR.mkdir(exist_ok=True)
+
+OUTPUT_FILE = OUTPUT_DIR / "leeftijd.json"
+
+def download():
+    print("Downloading Statbel income data...")
+  
+    response = requests.get(EXPORT_URL, timeout=120)
+    response.raise_for_status()
+
+    data = response.json()
+
+    with open(OUTPUT_FILE,"w",encoding="utf-8") as f:
+        json.dump(
+            data,
+            f,
+            ensure_ascii=False,
+            indent=2
+        )
+
+    print(f"Saved to {OUTPUT_FILE}")
+
+if __name__ == "__main__":
+        download()
